853ac296232cd68b1e46316d2e8f58d3434dbf3b
saimanmoreno <simaojmoreno@gmail.com>
2022-12-09 15:19:49 +0000

    some updates to ui

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -32,76 +32,92 @@
 
             </div>
 
-            <div class="py-2">
+            <div class="row">
+                <div class="py-2 col-md-8 px-md-5">
 
-                <!-- New Task Form -->
-                <form action="/task" method="POST" class="form-horizontal">
+                    <div class="py-5">
 
-                    {{ csrf_field() }}
+                        <!-- List Tasks -->
+                        @if (count($tasks) > 0)
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">Task Name</th>
+                                        <th scope="col">Created At</th>
+                                        <th scope="col">Due</th>
+                                        <th scope="col">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
 
-                    <!-- Task Name -->
+                                    @foreach ($tasks as $task)
+                                        <tr>
 
-                    <div class="form-group">
+                                            <th scope="row"> {{ $task->id }} </th>
+                                            <td>{{ $task->name }}</td>
+                                            <td>{{ $task->created_at }}</td>
+                                            <td> Data </td>
+                                            <td>
+                                                <form action="/task/{{ $task->id }}" method="post">
+                                                    {{ csrf_field() }}
+                                                    {{ method_field('DELETE') }}
+                                                    <!-- We can spoof a DELETE request -->
 
-                        <div class="form-group mb-2">
+                                                    <button>Delete Task</button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @endforeach
 
-                            <label for="task" class="sr-only">Task</label>
+                                </tbody>
+                            </table>
+                        @else
+                            <h4>No tasks yet! Create one first...</h4>
+                        @endif
 
-                            <input type="text" name="name" id="task-name" class="form-control"
-                                placeholder="Task name here">
+                    </div>
+                </div>
+
+                <div class="py-5 col-md-4 justify-content-center">
+
+                    <!-- New Task Form -->
+                    <form action="/task" method="POST" class="form-horizontal">
+
+                        {{ csrf_field() }}
+
+                        <!-- Task Name -->
+
+                        <div class="form-group border p-md-5">
+
+                            <div class="form-group mb-2">
+
+                                <label for="task" class="sr-only">Task Name</label>
+
+                                <input type="text" name="name" id="task-name" class="form-control"
+                                    placeholder="Task name here">
+
+                            </div>
+
+                            <div class="form-group mb-2">
+
+                                <label for="due-date" class="sr-only">Due Date</label>
+
+                                <input type="date" name="due-date" id="due-date" class="form-control"
+                                    placeholder="Due Date">
+
+                            </div>
+
+                            <!-- Add Task Button -->
+                            <button type="submit" class="btn btn-primary">
+                                <i class="fa fa-plus"></i> Add Task
+                            </button>
 
                         </div>
 
-                        <!-- Add Task Button -->
-                        <button type="submit" class="btn btn-primary">
-                            <i class="fa fa-plus"></i> Add Task
-                        </button>
+                    </form>
 
-                    </div>
-
-                </form>
-
-            </div>
-
-            <div class="py-5">
-
-                <!-- List Tasks -->
-                @if (count($tasks) > 0)
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Task Name</th>
-                                <th scope="col">Created At</th>
-                                <th scope="col">Due</th>
-                                <th scope="col">Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-
-                            @foreach ($tasks as $task)
-                                <tr>
-
-                                    <th scope="row"> {{ $task->id }} </th>
-                                    <td>{{ $task->name }}</td>
-                                    <td>{{ $task->created_at }}</td>
-                                    <td> Data </td>
-                                    <td>
-                                        <form action="/task/{{ $task->id }}" method="post">
-                                            {{ csrf_field() }}
-                                            {{ method_field('DELETE') }}
-                                            <!-- We can spoof a DELETE request -->
-
-                                            <button>Delete Task</button>
-                                        </form>
-                                    </td>
-                                </tr>
-                            @endforeach
-
-                        </tbody>
-                    </table>
-                @endif
-
+                </div>
             </div>
 
         </div>
